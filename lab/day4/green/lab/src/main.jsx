39350564669ac3555ee09509de2fc71e5ecdf884
Alper Atay <aatay@Alpers-MacBook-Pro.local>
2026-01-21 11:58:51 +0300

    main.jsx

--- lab/day4/green/lab/src/main.jsx
+++ lab/day4/green/lab/src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { StyledEngineProvider } from '@mui/material/styles'
+import { theme } from './theme/theme'
+
+// TODO(Day4): explain injectFirst when mixing Tailwind + MUI (Strangler).
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 )
